2c63844c2f85bc3fac991ae790ff3a92c3cf98dd
ArieL19999 <125905052+ArieL19999@users.noreply.github.com>
2023-08-08 13:33:41 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+
+
+SCORES_FILE_NAME="scores.txt"
+USER_NAME="User"
+BAD_RETURN_CODE=0
+HOST_IP="......."
+HOST_FLASK_PORT=5000
+HOME_URL=f"http://{HOST_IP}:{HOST_FLASK_PORT}/"
+
+ELEMENT_ID_SCORE="score"
+
