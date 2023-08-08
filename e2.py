0f2c847410ffba1844c55c533d953cbd2d389ff5
ArieL19999 <125905052+ArieL19999@users.noreply.github.com>
2023-08-08 13:34:27 +0300

    Update e2.py

--- e2.py
+++ e2.py
@@ -1,15 +1,13 @@
 
 from selenium import webdriver
 
-import Utils
+import settings
 import os
 
 def test_scores_service(app_url, element_id):
-  # Open a web browser and navigate to a webpage
   driver = webdriver.Chrome()
   driver.get(app_url)
 
-  # Find an element with the ID "element-id"
   element = driver.find_element_by_id(element_id)
   element_value = element.text()
   print(f"Got element value for score = {element_value}")
